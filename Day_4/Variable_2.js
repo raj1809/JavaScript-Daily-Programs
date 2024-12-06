5dcb922002be8ca5da41738017fc1bab55a64730
raj1809 <rajtheking147@gmail.com>
2024-12-06 19:59:38 +0530

    const exmple

--- Day_4/Variable_2.js
+++ Day_4/Variable_2.js
@@ -6,3 +6,8 @@ lastName = 'cena';
 
 console.log(firstName);
 console.log(lastName);
+
+const christmas_2024 = '25/12/2024';
+   // christmas_2024 = '25/11/2024' ;  can't re-assign value to a constant variable
+
+console.log(christmas_2024);
